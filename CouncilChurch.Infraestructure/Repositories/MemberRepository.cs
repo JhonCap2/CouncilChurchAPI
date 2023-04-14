33243c2d17e0cf3ea839f4942cbd3e925a641c19
Jhon Adames <jhon.adames@ceo.do>
2023-04-14 19:17:21 -0400

    Repositorys listos

--- CouncilChurch.Infraestructure/Repositories/MemberRepository.cs
+++ CouncilChurch.Infraestructure/Repositories/MemberRepository.cs
@@ -27,34 +27,42 @@ namespace CouncilChurch.Infraestructure.Repositories
             return rows > 0;
         }
 
-        public async Task<Member> GetMember(Guid id)
+        public async Task<Core.Entities.Member> GetMember(Guid id)
         {
-            var member = await _context.Members.Include(x => x.IdProfession)
-                    .Include(x => x.IdAddress)
+            var church = await _context.Members
                     .Include(x => x.IdChurch)
+                    .Include(x => x.IdAddress)
+                    .Include(x => x.IdProfession)
+                    .Include(x => x.IdCivilStates)
                     .SingleOrDefaultAsync(x => x.IdMembers == id);
-            return member;
+            return church;
         }
 
-        public async Task<IEnumerable<Member>> GetMembers()
+        public async Task<IEnumerable<Core.Entities.Member>> GetMembers()
         {
             var members = await _context.Members.ToListAsync();
             return members;
         }
 
-        public async Task InsertMember(Member newmember)
+        public async Task InsertMember(Core.Entities.Member newmember)
         {
             await _context.Members.AddAsync(newmember);
             _context.SaveChanges();
         }
 
-        public async Task<bool> UpdateMember(Member member)
+        public async Task<bool> UpdateMember(Core.Entities.Member member)
         {
-            var currentchurch = await GetMember(member.);
-            currentchurch.NameChurch = member.NameChurch;
+            var currentchurch = await GetMember(member.IdMembers);
+            currentchurch.IdChurch = member.IdChurch;
+            currentchurch.IdProfession = member.IdProfession;
             currentchurch.IdAddress = member.IdAddress;
-            currentchurch.IdCouncil = member.IdCouncil;
-            currentchurch.Web = member.Web;
+            currentchurch.IdCivilStates = member.IdCivilStates;
+            currentchurch.FirstName = member.FirstName;
+            currentchurch.SecondName = member.SecondName;
+            currentchurch.FirstSurname = member.FirstSurname;
+            currentchurch.SecondSurname = member.SecondSurname;
+            currentchurch.Nickname = member.Nickname;
+            currentchurch.Birthdate = member.Birthdate;
 
             int rows = await _context.SaveChangesAsync();
             return rows > 0;
